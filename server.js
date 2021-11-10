70200538d6b79e54ca53f94a0e443a83c01e4859
lsendlein <71516446+lsendlein@users.noreply.github.com>
2021-11-10 10:34:48 -0500

    Update server.js

--- server.js
+++ server.js
@@ -115,7 +115,9 @@ async function processMessage(url) {
     if (url.includes("pokemon.gameinfo")) {
         let pokemon = async() => {
             const browser = await puppeteer.launch({
-                executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
+                // if you uncomment below it will open in google chrome instead of chromium but you will need 
+                // to check that your path to chrome on your computer matches bc it could be diff
+                // executablePath: "C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe",
                 headless: false,
             });
             const page = await browser.newPage();
